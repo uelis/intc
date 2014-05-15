d4a1a130bcd0a1a0bed0895ba002d3712e5ee64d
Ulrich Schoepp <schoepp@tcs.ifi.lmu.de>
2014-05-15 16:48:53 +0200

    Use native recursion in Haskell example

--- Examples/Comparison/euler.hs
+++ Examples/Comparison/euler.hs
@@ -2,8 +2,6 @@ module Main where
 import Prelude hiding (tail, and, or, abs, signum)
 import Data.Int
 
-fix f = f (fix f)
-
 cons x xs =
   \i -> if i == 0 then x else xs (i-1)
 
@@ -11,7 +9,7 @@ head xs = xs 0
 
 tail xs = \i -> xs (i+1)
 
-aux = fix (\a -> \n -> cons 1 (cons n (cons 1 (a (n+2)))))
+aux n = cons 1 (cons n (cons 1 (aux (n+2))))
 
 eContFrac = cons 2 (aux 2)
 
@@ -23,8 +21,7 @@ and x y =
 or x y = 
   if x then True else y
 
-ratTrans =
-  fix (\ratTrans -> \av bv cv dv xs iv ->
+ratTrans av bv cv dv xs iv =
              let qv = if dv == 0 then 1 else bv `div` dv in
              if and (or (signum cv == signum dv) (abs cv < abs dv)) 
                       (and ((cv+dv)*qv < av+bv+1) 
@@ -34,13 +31,12 @@ ratTrans =
                       ratTrans cv dv (av-qv*cv) (bv-qv*dv) xs (iv-1))
              else 
                  let xv = xs 0 in 
-                 ratTrans bv (av+xv*bv) dv (cv+xv*dv) (tail xs) iv)
+                 ratTrans bv (av+xv*bv) dv (cv+xv*dv) (tail xs) iv
 
-toDigits = 
-  fix (\toDigits -> \l ->
-   \i -> if i == 0 then l i else toDigits (ratTrans 10 0 0 1 (tail l)) (i-1))
+toDigits l i = 
+   if i == 0 then l i else toDigits (ratTrans 10 0 0 1 (tail l)) (i-1)
 
--- e :: Int64 -> Int64
+--e :: Int64 -> Int64
 e = toDigits eContFrac
 
 main =
