7e39c6950ebe96b8149c6c79b2d43e2085c0eab3
Ulrich Schoepp <schoepp@tcs.ifi.lmu.de>
2015-01-15 17:00:21 +0100

    Failure tests

--- test.ml
+++ test.ml
@@ -1,9 +1,31 @@
 open Core.Std
 open OUnit2
+  
+let files_in_dir dir_name extension =
+  let files = ref [] in
+  let dir = Unix.opendir dir_name in
+  begin
+    try
+      while true do
+        let file = Unix.readdir dir in
+        if String.is_suffix ~suffix:extension file then
+          files := (dir_name ^ "/" ^ file) :: !files
+      done
+    with
+    | End_of_file ->
+      Unix.closedir dir
+  end;
+  !files
 
 let parse s =
   let lexbuf = Lexing.from_string s in
   Parser.decls Lexer.main lexbuf
+
+let read_decls filename =
+  filename
+  |> In_channel.read_all
+  |> parse
+  |> Decl.expand_all
 
 let compile = function Decl.TermDecl(x, ast) ->
   ast
@@ -39,9 +61,8 @@ let run_llvm test_ctx llvm =
   res
 
 let run_int_main test_ctx filename =
-  let i = In_channel.read_all filename in
-  parse i
-  |> Decl.expand_all
+  filename
+  |> read_decls
   |> List.filter ~f:(function Decl.TermDecl(f, _) -> Ident.to_string f = "main")
   |> List.map ~f:(fun d -> d |> compile |> run_llvm test_ctx)
        
@@ -54,26 +75,33 @@ let test_of_file filename =
        assert_equal actual expected
      | _ ->
        assert_failure "compilation error or more than one main definition")
+  
+let test_fail_file filename =
+  filename >::
+  ( fun _ ->
+     let fails =
+       begin
+         try
+           ignore (filename
+                   |> read_decls
+                   |> List.map ~f:compile);
+           false
+         with
+         | _ -> true
+       end in
+     if not fails then
+       assert_failure (filename ^ " should not be accepted."))
 
-let testdir = "Tests/"
-let testfiles = [
-  "alloc.int";
-  "alloc_pairs.int";
-  "arrays.int";
-  "fib0.int";
-  "fib_rec_tailrec.int";
-  "fib_cps.int";
-  "euler.int";
-  "fac.int";
-  "box.int";
-  "typing.int";
-]
-
+let success_tests = files_in_dir "Tests" ".int"
+let fail_tests = files_in_dir "Tests/Should_fail" ".int"
+    
 let suite =
-  "compilation tests" >:::
-  (testfiles
-   |> List.map ~f:(fun fn -> testdir ^ fn)
-   |> List.map ~f:test_of_file)
+  "intc tests" >:::
+    ["success tests" >:::
+     (success_tests |> List.map ~f:test_of_file);
+     "fail tests" >:::
+     (fail_tests |> List.map ~f:test_fail_file)
+    ]
 
 let () =
   run_test_tt_main suite
